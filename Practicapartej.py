cb491ba0990f1abc70815d04f7e040276d04f10c
ErYavi <96310629+ErYavi@users.noreply.github.com>
2023-01-28 16:45:35 +0100

    Update Practicapartej.py

--- Practicapartej.py
+++ Practicapartej.py
@@ -1,10 +1,9 @@
 import numpy as np
-import menu
 import cv2
 from PIL import ImageGrab, ImageOps, Image
 import pyautogui as py
 import time
-import win32gui
+import win32gui as wg
 import win32ui
 import win32api
 import win32con
@@ -13,43 +12,78 @@ import pyocr
 import pyocr.builders
 import math
 from Objetos import *
+import copy
 
-# Coordenadas de la esquina superior izquierda del cuadrado
-x1, y1 = menu.xa, menu.ya
-# Coordenadas de la esquina inferior derecha del cuadrado
-x2, y2 = menu.xb, menu.yb
+xa, ya, xb, yb = 0, 0, 0, 0
+switch = False
+option = ""
 
 
-# COGE LA PARTE DE LA PANTALLA DEL COMBATE
-#imgOriginal = cv2.imread('prueba1.jpg', cv2.IMREAD_COLOR)
-#img = imgOriginal[125:656,0:640]
-#imgray = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
-#cv2.imshow("imagen", imgray)
+def dibujar(event, x, y, flags, param):
+    global xa, ya, xb, yb, switch
+    if event == cv2.EVENT_LBUTTONDOWN:
+        xa, ya = x, y
+        switch = False
+    if event == cv2.EVENT_LBUTTONUP:
+        xb, yb = x, y
+        switch = True
+
+
+def aceptar(title='', text='', button='OK'):
+    py.alert(title=title, text=text, button=button)
+    win32handles = py.getWindowsWithTitle(title)
+    if win32handles:
+        wg.SetForegroundWindow(win32handles[0])
+
+
+pantalla = cv2.cvtColor(np.array(py.screenshot()), cv2.COLOR_BGR2RGB)
+
+cv2.namedWindow("screen", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("screen", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.setMouseCallback("screen", dibujar)
+while option != "yes":
+    img = copy.copy(pantalla)
+    if switch == True:
+        cv2.rectangle(img, (xa, ya), (xb, yb), (0, 0, 200), 1)
+        cv2.imshow("screen", img)
+        option = py.confirm(
+            "Game area correctly selected?\nIt must include all the game screen plus your attacks and pokémon, but must NOT include any fo the browser borders or the PokemonShowDown banner. Otherwise the bot will not work\nDO NOT MOVE THE MOUSE AFTER PRESSING YES", "Confirm", ["yes", "no"])
+        if option != "yes":
+            switch = False
+            aceptar(title="SELECT GAME AREA",
+                    text="Select the game area, then press [Accept]")
+    cv2.imshow("screen", img)
+    if option != "yes" and switch != True:
+        aceptar(title="SELECT GAME AREA",
+                text="Select the game area, then press [Accept]")
+    cv2.waitKey(1)
+cv2.destroyAllWindows()
+time.sleep(1)
+print("sin ordenar:\n("+str(xa)+", "+str(ya)+")("+str(xb)+", "+str(yb)+")")
+
+if xa > xb:
+    aux = copy.copy(xa)
+    xa = copy.copy(xb)
+    xb = copy.copy(aux)
+if ya > yb:
+    aux = copy.copy(ya)
+    ya = copy.copy(yb)
+    yb = copy.copy(aux)
+
+print("ordenadas:\n("+str(xa)+", "+str(ya)+")("+str(xb)+", "+str(yb)+")")
 
 nPokemon = 6
 nPokemonRival = 6
 
 # Coordenadas para leer datos pokemon
-x, y = 210+x1, 205+y1
-# while True:            #
-# Take screenshot of specific region
-#imAntes = np.array(py.screenshot(region=(0, 125, 640, 656)))
-imAntes = np.array(py.screenshot(region=(x1, y1, x2, y2)))
+x, y = 200+xa, 278+ya
 win32api.SetCursorPos((x, y))
-time.sleep(0.5)
-#imAtributos = np.array(py.screenshot(region=(0, 125, 640, 656)))
-imAtributos = np.array(py.screenshot(region=(x1, y1, x2, y2)))
-imResta = cv2.subtract(imAntes, imAtributos)
-#mask = imResta
-# for i in range(imResta):
-#    for j in range(imResta):
-#        if imResta == 0:
-#            mask[i][j] = 0
-im = Image.fromarray(imResta)
-im.show()
+time.sleep(2)
+screen = py.screenshot(region=(xa, ya, xb-xa, yb-ya))
+screen.show()
 
 # guardar datos
-texto = pytesseract.image_to_string(imResta, lang='eng')
+texto = pytesseract.image_to_string(screen, lang='eng')
 print(texto)
 
 
@@ -134,7 +168,7 @@ print(pokemon)
 ##canny = cv2.Canny(imgray, 100, 200)
 ##cv2.imshow("canny", canny)
 ##contornos, _ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-##imdraw = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
+##imdraw = cv2.cvtColor(canny, cv2.COLOR_GRAybBGR)
 # for cnt in contornos:
 ##    approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
 # if len(approx) == 4:
@@ -158,7 +192,7 @@ print(pokemon)
 #    win32gui.InvalidateRect(hwnd, monitor, True) # Refresh the entire monitor
 # calcularArea()
 
-# #cuadrado = py.drawRectangle((x1, y1), (x2, y2), width=2)
+# #cuadrado = py.drawRectangle((xa, ya), (xb, yb), width=2)
 
 # class ataque:
 # def __init__(tipo, daño, precision, estado):
